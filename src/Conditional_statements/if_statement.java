5d52cfe820809abaecdd035dbeda0fa9cadbd027
trishali3 <ramisettitrishali@gmail.com>
2024-06-25 19:21:18 +0530

    Update if_statement.java

--- src/Conditional_statements/if_statement.java
+++ src/Conditional_statements/if_statement.java
@@ -4,6 +4,9 @@ public class if_statement{
 public static void main(String[] args) {
 	if(5<=8) {
 		System.out.println("Hello world");
+                System.out.println(" hi this is changr made by tl from github");
+		
+
 	}
 		
 }	
